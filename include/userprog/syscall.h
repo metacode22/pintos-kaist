6d5304df663c5d96c5b210c0c48ee999609802fb
Ubuntu <ubuntu@ip-172-31-45-147.ap-northeast-2.compute.internal>
2022-06-02 13:27:17 +0000

    feat : we have to implement fork function

--- include/userprog/syscall.h
+++ include/userprog/syscall.h
@@ -14,5 +14,16 @@ void exit (int status);
 tid_t fork (const char *thread_name, struct intr_frame *f);
 bool create (const char *file, unsigned initial_size);
 bool remove (const char *file);
+int write (int fd, const void *buffer, unsigned size);
+int open (const char *file);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned size);
+void seek(int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
+
+int add_file_to_fd_table (struct file *file);
+struct file *get_file_from_fd_table (int fd);
+void close_file_from_fd_table (int fd);
 
 #endif /* userprog/syscall.h */
